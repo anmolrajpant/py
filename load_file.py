46fa9d7bc44960aa7d70346e1692bb1c216445c6
Anmol Raj Pant <anmolrajpant@hotmail.com>
2018-07-26 10:04:00 +0545

    initial data cleanising

---
+++ load_file.py
@@ -0,0 +1,18 @@
+file = open('price.txt','r')
+#print(file.read())
+data = []
+line = file.readline()
+while (line != ''):
+	line = line.replace('\n', '')
+	row = line.split(',')
+	for elem in row:
+		index = row.index(elem)
+		row[index] = elem.strip()
+	data.append(row)
+	#print(row)
+	line = file.readline()
+print(data)
+
+#average price
+#option to choose item
+#print recipt
