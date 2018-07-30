1aebbda8e0f068fb713fd5c62ed3b53f070f6a9a
Anmol Raj Pant <anmolrajpant@hotmail.com>
2018-07-30 10:11:08 +0545

    average calc using zip

--- arp_store.py
+++ arp_store.py
@@ -1,2 +1,7 @@
 veg = ['','Tomato', 'Potato', 'Onion', 'Pumpkin', 'Banana', 'Garlic', 'Ginger', 'Mushroom', 'Celery', 'Cabbage']
-price = ['','45', '']
+maxPrice = [0,40,55,50,40,90,150,110,180,110,50]
+minPrice = [0,50,60,60,50,100,200,120,200,120,60]
+
+avgPrice=[((x + y)/2) for x, y in zip(maxPrice, minPrice)]
+
+print(avgPrice)
